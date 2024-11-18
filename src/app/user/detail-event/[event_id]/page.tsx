59bd1ae881a514891198d9ec82f0237a941b085c
guspan-tanadi <guspan.tanadi@gmail.com>
2024-11-18 13:24:48 +0700

    chore: param router

--- src/app/user/detail-event/[event_id]/page.tsx
+++ src/app/user/detail-event/[event_id]/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState, useRef } from "react";
 import Cookies from "js-cookie";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
-import { useRouter, useParams } from "next/navigation";
+import { useRouter } from "next/router";
 import { Events, Discounts, Reviews } from "@/models/models";
 import LoadingSpinner from "@/components/LoadingSpinner";
 import MyNavbar from "@/components/MyNavbar";
@@ -29,9 +29,9 @@ type User = {
   role: string;
 };
 
-function Page() {
-  const { event_id } = useParams();
+function DetailEventPage() {
   const router = useRouter();
+  const { event_id } = router.query;
   const MySwal = withReactContent(Swal);
 
   const [event, setEvent] = useState<Events | null>(null);
@@ -408,4 +408,4 @@ function Page() {
   );
 }
 
-export default Page;
+export default DetailEventPage;
